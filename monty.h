0a9ae352958c0cc6765ea1aeedf601114d16fa1d
kim-thuita <kevinkoech265@gmail.com>
2023-05-24 21:47:48 +0300

    function switches two numbers stacks

--- monty.h
+++ monty.h
@@ -40,6 +40,6 @@ typedef struct instruction_s
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack **stack, unsigned int line_number);
-
+void swap(stack_t **stack, unsigned int line_number);
 
 #endif
