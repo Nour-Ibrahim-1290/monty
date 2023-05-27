702468f6e384526b1fb43820c8a610dd00bfaa79
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-05-27 23:22:51 +0200

    fix issue 10

--- pchar.c
+++ pchar.c
@@ -2,13 +2,29 @@
 
 
 /**
- * pchar - 
- * @stack:
- * @line_number
+ * pchar - print values as chars if they are
+ * @stack: linear structure we're working on
+ * @line_number: #line in test file
  *
+ * Return: void
  */
 void pchar(stack_t **stack, unsigned int line_number)
 {
-	(void) **stack;
-	(void) line_number;
+	int value;
+
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	value = (*stack)->n;
+
+	if (value < 0 || value > 127)
+	{
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	putchar(value);
 }
